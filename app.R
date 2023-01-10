934b6d1b1351314afffbaa3e47f687d4c635be9b
Laurentiuq <gheorghe-laurentiu.ghetoiu@s.unibuc.ro>
2023-01-10 18:52:22 +0200

    hasurare probabilitate

--- app.R
+++ app.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 library(shinyjs)
-library(shinydashboard)
+# library(shinydashboard)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -30,27 +30,44 @@ ui <- fluidPage(
                         
                         ),
             helpText(h3("A si B")),
-            numericInput("A", h4("A"), value = 0),
-            numericInput("B", h4("B"), value= 0)
+            numericInput("A", h1("A"), value = 0),
+            numericInput("B", h1("B"), value= 0)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           box(id = "beta", width = '1000px',
+           box(id = "beta", width = '800px',
                numericInput("alpha_",h3("alpha"),min=0.1, value = 0.1, step=0.1),
                numericInput("beta_", h3("beta"),min=0.1, value = 0.1, step=0.1)),
-           plotOutput("functii")
+           plotOutput("functii"),
+           fluidRow(
+             column(6,
+                    div(style = "height:20px; font-size:25px; color: rgb(9, 233, 1); margin-bottom: 20px; margin-top: 140px; font-weight: bold;", textOutput("Px_a_val")),
+                    div(style = "height:20px; font-size:25px; color: orange; margin-bottom: 20px; font-weight: bold;", textOutput("Px_b_val")),
+                    div(style = "height:20px; font-size:25px; color: blue; font-weight: bold;", textOutput("Px_a_b_val"))
+             ),
+             column(6,
+                    plotOutput("Px_a")
+             )
+           )
         )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
     # il folosim pentru a retine ultima variabila aleasa
     lastChoice <- "null"
     #input$var e var din radioButtons si returneza nr variabilei alese
     observeEvent(input$var, {
       if(input$var == 1){
+        
+        updateNumericInput(session, "A", label = "A", 
+                          value=0, min = 0, max = 1, step = 1
+        )
+        updateNumericInput(session, "B", label = "B", 
+                           value=0, min = 0, max = 1, step = 1
+        )
         if(lastChoice != "null")
           shinyjs::hide(id=lastChoice)
         shinyjs::show(id="beta")
@@ -62,12 +79,64 @@ server <- function(input, output) {
         # ylim = val de pe Oy
         output$functii <- renderPlot({par(mfrow = c(1,2))
           curve(dbeta(x, shape1 = input$alpha_, shape2 = input$beta_), 0, 1, ylim = c(0, 2.5),
-                ylab = "f(x)", col = "red", lwd = 2, main = "Functia de densitate")
-          curve(pbeta(x, shape1 = input$alpha_, shape2 = input$beta_), 0 , 1, ylim = c(0,1), type = "l", main = "Functia de repartitie", ylab="F(x)", lwd = 2, col = "red")
+                ylab = "f(x)", col = "black", lwd = 2, main = "Functia de densitate")
+          curve(pbeta(x, shape1 = input$alpha_, shape2 = input$beta_), 0 , 1, ylim = c(0,1), type = "l", main = "Functia de repartitie", ylab="F(x)", lwd = 2, col = "black")
           })
+        output$Px_a_val <- renderText({
+          paste("P(X <= A = P(X <=", input$A, ") =", round(pbeta(input$A, shape1= input$alpha_, shape2 = input$beta_), digits = 3))
+        })
+        output$Px_b_val <- renderText({
+          paste("P( X> = B = P(X >=", input$B, ") =", 1- round(pbeta(input$B, shape1= input$alpha_, shape2=input$beta_), digits = 3))
+        })
+        output$Px_a_b_val <- renderText({
+          paste("P( A <= X <= B = P(", input$A, "<= X <=", input$B, ") =", round((pbeta(input$B, shape1 = input$alpha_, shape2=input$beta_) - pbeta(input$A, shape1=input$alpha_, shape2=input$beta_)), digits = 3))
+        })
+
+        tryCatch(expr = {
+         # mai facem un grafic pentru functia de repartitie
+          output$Px_a <- renderPlot({curve(pbeta(x, shape1=input$alpha_, shape2=input$beta_),0,1,ylim=c(0,1), type="l", main="P(X <= A), P(X >= B) si P(A <= X <= B)", ylab="F(x)", lwd = 2, col = "black")
           
+         
+        # coloram portiunile de sub grafic
+          x <- seq(0, input$A, by =0.01)
+          x1 <- c(x, seq(input$A, 0, by =-0.01))
+          y <- c(c(pbeta(x, shape1 = input$alpha_, shape2= input$beta_)), seq(0,0,length = length(x)))
+          polygon(x1, y, col = "green")
+  
+          cat(file=stderr(), paste0("lastChoice: ",lastChoice, "\n"))
+  
+  
+          x <- seq(input$B, 1, by =0.01)
+          x1 <- c(x, seq(1, input$B, by =-0.01))
+          y <- c(c(pbeta(x, shape1= input$alpha_, shape2 = input$beta_)), seq(0,0, length = length(x)))
+          polygon(x1, y, col = "orange")
+  
+  
+          x <- seq(input$A, input$B, by =0.01)
+          x1 <- c(x, seq(input$B, input$A, by =-0.01))
+          y <- c(c(pbeta(x, shape1 = input$alpha_, shape2 = input$beta_)), seq(0,0, length = length(x)))
+          polygon(x1, y, col = "blue")
+          })
+        },
+        
+          error = function(e){
+            message('Valoare incorecta pentru A sau B')
+            print(e)
+          },
+          warning = function(w){
+            message('Test')
+            print(w)
+          }
+        )
+        
+        
+        
       }
+      
+      
+      
       if(input$var==2){
+        plot.new();
         shinyjs::hide(id=lastChoice)
         
         cat(file=stderr(), paste0("lastChoice: ",lastChoice, "\n"))
